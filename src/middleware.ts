8ab4b23c0051c1b9cec30dfca7db552968e1f797
Ankit Kumar <ak8415129@gmail.com>
2025-01-08 19:44:13 +0530

    implement middleware

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+
+  const isPublicPath = path === '/login' || path === '/signup'
+
+  const token = request.cookies.get('token')?.value || ''
+
+  // if we are login means having token token no need to see public path
+  if(isPublicPath && token) {
+    return NextResponse.redirect(new URL('/', request.nextUrl))
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/login', request.nextUrl))
+  }
+    
+}
+
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/profile',
+    '/profile/:id',
+    '/login',
+    '/signup',
+    
+  ]
+}
