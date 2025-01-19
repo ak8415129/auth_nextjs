44e5d0d5454c637fe4089a7e37a038fb1df65b50
Ankit Kumar <ak8415129@gmail.com>
2025-01-19 20:28:53 +0530

    learn middleware matchinf parths

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,7 +21,7 @@ export function middleware(request: NextRequest) {
 }
 
  
-// See "Matching Paths" below to learn more
+// See "Matching Paths" below to learn more !
 export const config = {
   matcher: [
     '/',
